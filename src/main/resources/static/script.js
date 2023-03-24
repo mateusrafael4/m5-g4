8850fbd1e450760681246b84ffe05f6513e861ae
GabrielNhoncanse <gabriel.nhoncanse@sou.inteli.edu.br>
2023-03-24 10:54:49 -0300

    Atualização da implementação do mapa no front

--- src/main/resources/static/script.js
+++ src/main/resources/static/script.js
@@ -1,3 +1,11 @@
+mapboxgl.accessToken = 'pk.eyJ1Ijoic29sZW1uIiwiYSI6ImNsZmlvbDBibjBrNTg0M25taG1xM2x2YXIifQ.aiMQpTd20YpCaWJfL5BmIg';
+var map = new mapboxgl.Map({
+    container: 'map',
+    style: 'mapbox://styles/mapbox/streets-v11',
+    center: [-48.63, -22.50], // lon , lat
+    zoom: 6 // initial map zoom level
+});
+
 function enviarDados() { // botão que enviará os inputs do front para o back
     var lat_str = document.getElementById("lat_str").value;
     var lon_str = document.getElementById("lon_str").value;
@@ -30,8 +38,17 @@ function enviarDados() { // botão que enviará os inputs do front para o back
     fetch(URL, postRequest)
         .then(resposta => { if (!resposta.ok) throw Error(resposta.status); return resposta; })
         .then(resposta => resposta.json());
-    //.then(jsonResponse => inicializar(jsonResponse))
-    // .catch(function(error) { paragrafoMensagem.innerHTML = 'Erro ao criar novo professor (código ' + error.message + ')'; } );
+    
+    var lat_center = (parseInt(mbr_lat_end) + parseInt(mbr_lat_str)) * 0.5;
+    var lon_center = (parseInt(mbr_lon_end) + parseInt(mbr_lon_str)) * 0.5;
+
+    mapboxgl.accessToken = 'pk.eyJ1Ijoic29sZW1uIiwiYSI6ImNsZmlvbDBibjBrNTg0M25taG1xM2x2YXIifQ.aiMQpTd20YpCaWJfL5BmIg';
+    var map = new mapboxgl.Map({
+        container: 'map',
+        style: 'mapbox://styles/mapbox/streets-v11',
+        center: [lon_center, lat_center], // lon, lat
+        zoom: 6 // initial map zoom level
+    });
 }
 
 var nodes;
@@ -54,10 +71,49 @@ fetch('http://localhost:8080/coordinates/Data', { // acessa a rota get/data cria
 // mapa
 
 
-mapboxgl.accessToken = 'pk.eyJ1Ijoic29sZW1uIiwiYSI6ImNsZmlvbDBibjBrNTg0M25taG1xM2x2YXIifQ.aiMQpTd20YpCaWJfL5BmIg';
-var map = new mapboxgl.Map({
-    container: 'map',
-    style: 'mapbox://styles/mapbox/streets-v11',
-    center: [-48.63, -22.50], // initial map center
-    zoom: 6 // initial map zoom level
-});
+
+
+
+// map.on('load', function () {
+//     // add markers for each coordinate
+//     var coordinates = [
+//         [-74.00597, 40.71427], // New York
+//         [-118.24368, 34.05223], // Los Angeles
+//         [-77.03637, 38.89511] // Washington DC
+//     ];
+//     coordinates.forEach(function (coordinate) {
+//         new mapboxgl.Marker()
+//             .setLngLat(coordinate)
+//             .addTo(map);
+//     });
+//     // create a GeoJSON object with the coordinates
+//     var geojson = {
+//         type: 'FeatureCollection',
+//         features: [{
+//             type: 'Feature',
+//             geometry: {
+//                 type: 'LineString',
+//                 coordinates: coordinates
+//             }
+//         }]
+//     };
+//     // add the GeoJSON object as a source for a line layer
+//     map.addSource('route', {
+//         type: 'geojson',
+//         data: geojson
+//     });
+//     // adiciona a aresta
+//     map.addLayer({
+//         id: 'route',
+//         type: 'line',
+//         source: 'route',
+//         layout: {
+//             'line-join': 'round',
+//             'line-cap': 'round'
+//         },
+//         paint: {
+//             'line-color': '#888',
+//             'line-width': 8
+//         }
+//     });
+// });
